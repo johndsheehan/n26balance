b61db254cb8a55cc54a049506ba73f6e08598426
John D Sheehan <john.d.sheehan@gmail.com>
2019-04-23 18:59:41 +0100

    add watch interval
    loop forever, querying every "interval" seconds
    
    Signed-off-by: John D Sheehan <john.d.sheehan@gmail.com>

--- cmd/n26balance/main.go
+++ cmd/n26balance/main.go
@@ -4,12 +4,15 @@ import (
 	"errors"
 	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/johndsheehan/n26balance/pkg/n26"
 	"github.com/spf13/cobra"
 
 	yaml "gopkg.in/yaml.v2"
 )
+
+const queryInterval = 600 * time.Second
 
 func n26CmdCreate() *cobra.Command {
 	cmd := &cobra.Command{
@@ -24,16 +27,38 @@ func n26CmdCreate() *cobra.Command {
 				return errors.New("no configuration file provided")
 			}
 
-			return n26CmdExecute(cfgFile)
+			watch, err := cmd.Flags().GetBool("watch")
+			if err != nil {
+				return errors.New("failed to determine watch value")
+			}
+
+			interval := queryInterval
+			if watch {
+				i, err := cmd.Flags().GetInt("interval")
+				if err != nil {
+					return errors.New("failed to determine interval value")
+				}
+
+				if i < 1 {
+					log.Printf("integer should be positive interval, using default: %d seconds",
+						queryInterval/(1000*time.Millisecond))
+				} else {
+					interval = time.Duration(i) * time.Second
+				}
+			}
+
+			return n26CmdExecute(cfgFile, watch, interval)
 		},
 	}
 
 	cmd.Flags().StringP("config", "c", "", "configuration file")
+	cmd.Flags().IntP("interval", "i", 0, "interval between queries")
+	cmd.Flags().BoolP("watch", "w", false, "query balance every \"interval\" minutes, report if changed")
 
 	return cmd
 }
 
-func n26CmdExecute(cfgFile string) error {
+func n26CmdExecute(cfgFile string, watch bool, interval time.Duration) error {
 	yml, err := ioutil.ReadFile(cfgFile)
 	if err != nil {
 		return err
@@ -57,6 +82,29 @@ func n26CmdExecute(cfgFile string) error {
 
 	log.Printf("available: %0.2f\n", balance.AvailableBalance)
 	log.Printf("usable: %0.2f\n", balance.UsableBalance)
+
+	if watch {
+		previous := balance.UsableBalance
+
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case _ = <-ticker.C:
+				balance, err = n26Client.Balance()
+				if err != nil {
+					return err
+				}
+
+				if balance.UsableBalance != previous {
+					log.Printf("available: %0.2f\n", balance.AvailableBalance)
+					log.Printf("usable: %0.2f\n", balance.UsableBalance)
+					previous = balance.UsableBalance
+				}
+			}
+		}
+	}
 
 	return nil
 }
